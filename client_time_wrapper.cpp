362f21070196bb600f5b75cdb9d9130255497f6f
mayermichael1 <michael.mayer2105@gmail.com>
2024-06-22 16:46:09 +0200

    working wrapper around client

--- client_time_wrapper.cpp
+++ client_time_wrapper.cpp
@@ -2,9 +2,14 @@
 #include <time.h>
 #include <unistd.h>
 
+#include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
+#include <fcntl.h>
+
 struct pipeStruct{
-    int write;
     int read;
+    int write;
 };
 int main(){ 
     pipeStruct writeToUUDPClient = {};
@@ -13,18 +18,27 @@ int main(){
     pid_t childPid = 0;
 
     childPid = fork();
+
     if(childPid < 0){
         printf("Process could not be created \n");
-    }else if(childPid == 0){
+    } else if(childPid == 0){ // child process here
+        close(STDOUT_FILENO);
+        close(writeToUUDPClient.write);
+        dup2(writeToUUDPClient.read, STDIN_FILENO);
 
-    }else{
-
+        char *program[3] = {(char*)"./uudpchat", (char*)"client", NULL};
+        execv(program[0], program);
+    } else{ // the parent process
+        close(writeToUUDPClient.read);
+        while(true){
+            time_t currentTime = time(NULL);
+            char *timeString = ctime(&currentTime);
+            write(writeToUUDPClient.write, timeString, strlen(timeString));
+            sleep(1);
+        }
+        kill(childPid, SIGTERM); // should work if not use 1
+        wait(NULL);
     }
 
-    while(1){
-        time_t currentTime = time(NULL);
-        printf("%s", ctime(&currentTime));
-        sleep(1);
-    }
     return 0;
 }
