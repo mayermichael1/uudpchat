6811652411f20d64f5dd60e7754fa71a7d8f813f
mayermichael1 <michael.mayer2105@gmail.com>
2024-06-24 22:19:38 +0200

    some refactor

--- client_time_wrapper.cpp
+++ client_time_wrapper.cpp
@@ -11,6 +11,28 @@ struct pipeStruct{
     int read;
     int write;
 };
+
+void childProcess(pipeStruct writeToUUDPClient){
+    close(STDOUT_FILENO);
+    close(writeToUUDPClient.write);
+    dup2(writeToUUDPClient.read, STDIN_FILENO);
+
+    char *program[3] = {(char*)"./uudpchat", (char*)"client", NULL};
+    execv(program[0], program);
+}
+
+void parentProcess(pipeStruct writeToUUDPClient, pid_t childPid){
+    close(writeToUUDPClient.read);
+    while(true){
+        time_t currentTime = time(NULL);
+        char *timeString = ctime(&currentTime);
+        write(writeToUUDPClient.write, timeString, strlen(timeString));
+        sleep(1);
+    }
+    kill(childPid, SIGTERM); // should work if not use 1
+    wait(NULL);
+}
+
 int main(){ 
     pipeStruct writeToUUDPClient = {};
 
@@ -22,22 +44,9 @@ int main(){
     if(childPid < 0){
         printf("Process could not be created \n");
     } else if(childPid == 0){ // child process here
-        close(STDOUT_FILENO);
-        close(writeToUUDPClient.write);
-        dup2(writeToUUDPClient.read, STDIN_FILENO);
-
-        char *program[3] = {(char*)"./uudpchat", (char*)"client", NULL};
-        execv(program[0], program);
+        childProcess(writeToUUDPClient);
     } else{ // the parent process
-        close(writeToUUDPClient.read);
-        while(true){
-            time_t currentTime = time(NULL);
-            char *timeString = ctime(&currentTime);
-            write(writeToUUDPClient.write, timeString, strlen(timeString));
-            sleep(1);
-        }
-        kill(childPid, SIGTERM); // should work if not use 1
-        wait(NULL);
+        parentProcess(writeToUUDPClient, childPid);
     }
 
     return 0;
